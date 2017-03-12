e06808d3259f411710e43e03bffd4acc5a362cc3
Colin T.A. Gray <colinta@gmail.com>
2017-03-11 19:51:42 -0700

    implement the rest of 'Box' (borders)

--- Sources/Components/Box.swift
+++ Sources/Components/Box.swift
@@ -3,18 +3,83 @@
 //
 
 class Box: ComponentLayout {
-    indirect enum Border {
-        case lame
-        case single
-        case double
-        case mixed
+    struct Border {
+        let dot: String
+        let topCap: String
+        let bottomCap: String
+        let leftCap: String
+        let rightCap: String
+        let tlCorner: String
+        let trCorner: String
+        let blCorner: String
+        let brCorner: String
+        let topSide: String
+        let bottomSide: String
+        let tbSide: String
+        let leftSide: String
+        let rightSide: String
+        let lrSide: String
+
+        init(
+            dot: String? = nil, topCap: String? = nil, bottomCap: String? = nil, leftCap: String? = nil, rightCap: String? = nil,
+            tlCorner: String, trCorner: String, blCorner: String, brCorner: String,
+            tbSide: String, topSide: String? = nil, bottomSide: String? = nil,
+            lrSide: String, leftSide: String? = nil, rightSide: String? = nil
+            )
+        {
+            self.tlCorner = tlCorner
+            self.trCorner = trCorner
+            self.blCorner = blCorner
+            self.brCorner = brCorner
+            self.tbSide = tbSide
+            self.lrSide = lrSide
+
+            self.dot = dot ?? tlCorner
+            self.topCap = topCap ?? tlCorner
+            self.bottomCap = bottomCap ?? blCorner
+            self.leftCap = leftCap ?? tlCorner
+            self.rightCap = rightCap ?? trCorner
+            self.topSide = topSide ?? tbSide
+            self.bottomSide = bottomSide ?? tbSide
+            self.leftSide = leftSide ?? lrSide
+            self.rightSide = rightSide ?? lrSide
+
+        }
+
+        static let lame = Border(
+            tlCorner: "+", trCorner: "+", blCorner: "+", brCorner: "+",
+            tbSide: "-",
+            lrSide: "|"
+            )
+        static let single = Border(
+            dot: "◻︎", topCap: "┬", bottomCap: "┴", leftCap: "├", rightCap: "┤",
+            tlCorner: "┌", trCorner: "┐", blCorner: "└", brCorner: "┘",
+            tbSide: "─", lrSide: "│"
+            )
+        static let double = Border(
+            dot: "◻︎", topCap: "╥", bottomCap: "╨", leftCap: "╟", rightCap: "╢",
+            tlCorner: "╔", trCorner: "╗", blCorner: "╚", brCorner: "╝",
+            tbSide: "═", lrSide: "║"
+            )
+        static let rounded = Border(
+            dot: "▢", topCap: "╷", bottomCap: "╵", leftCap: "╶", rightCap: "╴",
+            tlCorner: "╭", trCorner: "╮", blCorner: "╰", brCorner: "╯",
+            tbSide: "─", lrSide: "│"
+            )
+        static let bold = Border(
+            dot: "◼︎", topCap: "┳", bottomCap: "┻", leftCap: "┣", rightCap: "┫",
+            tlCorner: "┏", trCorner: "┓", blCorner: "┗", brCorner: "┛",
+            tbSide: "━", lrSide: "┃"
+            )
+        // static let double = Border(
+        // static let mixed = Border(
     }
 
     let size: Size
     let border: Border?
     let background: TextType?
 
-    init(_ location: Location, _ size: Size, border: Border? = nil, background: TextType? = nil, components: [Component]) {
+    init(_ location: Location, _ size: Size, border: Border? = nil, background: TextType? = nil, components: [Component] = []) {
         self.size = size
         self.border = border
         self.background = background
@@ -39,40 +104,64 @@ class Box: ComponentLayout {
             size = Size(width: max(0, screenSize.width - 2), height: max(0, screenSize.height - 2))
         }
 
-        if let background = background, size.height > 0 && size.width > 0 {
-            for y in 0 ..< size.height {
-                for x in 0 ..< size.width {
-                    buffer.write(background, x: x, y: y)
+        buffer.push(offset: borderOffset, clip: size) {
+            if let background = background {
+                for y in 0 ..< size.height {
+                    for x in 0 ..< size.width {
+                        buffer.write(background, x: x, y: y)
+                    }
+                }
+            }
+
+            for view in views.reversed() {
+                let viewSize = view.desiredSize().constrain(in: size)
+                let offset = view.location.origin(for: viewSize, in: size)
+                buffer.push(offset: offset, clip: viewSize) {
+                    view.render(in: buffer, size: viewSize)
                 }
             }
         }
 
-        for view in components {
-            guard let view = view as? ComponentView else { continue }
-
-            let viewSize = view.desiredSize().constrain(in: size)
-            let viewOffset = view.location.origin(for: viewSize, in: size)
-            let offset = Point(
-                x: viewOffset.x + borderOffset.x,
-                y: viewOffset.y + borderOffset.y
-                )
-            buffer.push(offset: offset) {
-                view.render(in: buffer, size: viewSize)
+        if let border = border {
+            let minX = 0, maxX = screenSize.width - 1
+            let minY = 0, maxY = screenSize.height - 1
+            switch (screenSize.width, screenSize.height) {
+            case (1, 1):
+                buffer.write(border.dot, x: minX, y: minY)
+            case (_, 1):
+                buffer.write(border.leftCap, x: minX, y: minY)
+                buffer.write(border.rightCap, x: maxX, y: minY)
+                if screenSize.width > 2 {
+                    for x in 1 ..< maxX {
+                        buffer.write(border.tbSide, x: x, y: minY)
+                    }
+                }
+            case (1, _):
+                buffer.write(border.topCap, x: minX, y: minY)
+                buffer.write(border.bottomCap, x: minX, y: maxY)
+                if screenSize.height > 2 {
+                    for y in 1 ..< maxY {
+                        buffer.write(border.lrSide, x: minX, y: y)
+                    }
+                }
+            default:
+                buffer.write(border.tlCorner, x: minX, y: minY)
+                buffer.write(border.trCorner, x: maxX, y: minY)
+                buffer.write(border.blCorner, x: minX, y: maxY)
+                buffer.write(border.brCorner, x: maxX, y: maxY)
+                if screenSize.width > 2 {
+                    for x in 1 ..< maxX {
+                        buffer.write(border.topSide, x: x, y: minY)
+                        buffer.write(border.bottomSide, x: x, y: maxY)
+                    }
+                }
+                if screenSize.height > 2 {
+                    for y in 1 ..< maxY {
+                        buffer.write(border.leftSide, x: minX, y: y)
+                        buffer.write(border.rightSide, x: maxX, y: y)
+                    }
+                }
             }
         }
-
-        if let border = border {
-        }
     }
-}
-
-extension Box.Border {
-    var tl: String { return "+" }
-    var tr: String { return "+" }
-    var bl: String { return "+" }
-    var br: String { return "+" }
-    var t: String { return "-" }
-    var b: String { return "-" }
-    var l: String { return "|" }
-    var r: String { return "|" }
 }
---
+++ Sources/Specs/BoxSpecs.swift
@@ -0,0 +1,50 @@
+////
+///  BoxSpecs.swift
+//
+
+
+struct BoxSpecs: Spec {
+    var name: String { return "BoxSpecs" }
+
+    func run(expect: (String) -> Expectations, done: @escaping () -> Void) {
+        let border = Box.Border(
+            dot: ".", topCap: "T", bottomCap: "U", leftCap: "L", rightCap: "R",
+            tlCorner: "!", trCorner: "@", blCorner: "#", brCorner: "$",
+            tbSide: "=", topSide: "_", bottomSide: "-",
+            lrSide: "|", leftSide: ">", rightSide: "<"
+            )
+        let borderDefaults = Box.Border(
+            tlCorner: "!", trCorner: "@", blCorner: "#", brCorner: "$",
+            tbSide: "-", lrSide: ">"
+            )
+        expect("outputs nothing")
+            .assertRenders(Box(.topLeft(), Size(width: 0, height: 0), border: border, background: "x"), "", "when width,height == 0")
+            .assertRenders(Box(.topLeft(), Size(width: 1, height: 0), border: border, background: "x"), "", "when height == 0")
+            .assertRenders(Box(.topLeft(), Size(width: 0, height: 1), border: border, background: "x"), "", "when width == 0")
+        expect("outputs dot")
+            .assertRenders(Box(.topLeft(), Size(width: 1, height: 1), border: border, background: "x"), ".")
+            .assertRenders(Box(.topLeft(), Size(width: 1, height: 1), border: borderDefaults, background: "x"), "!", "defaults")
+        expect("outputs top/bottom caps")
+            .assertRenders(Box(.topLeft(), Size(width: 1, height: 3), border: border, background: "x"), "T\n|\nU")
+            .assertRenders(Box(.topLeft(), Size(width: 1, height: 3), border: borderDefaults, background: "x"), "!\n|\n#", "defaults")
+        expect("outputs left/right caps")
+            .assertRenders(Box(.topLeft(), Size(width: 3, height: 1), border: border, background: "x"), "L=R")
+            .assertRenders(Box(.topLeft(), Size(width: 3, height: 1), border: borderDefaults, background: "x"), "!-@", "defaults")
+        expect("outputs corners")
+            .assertRenders(Box(.topLeft(), Size(width: 2, height: 2), border: border, background: "x"), "!@\n#$")
+        expect("outputs box with background")
+            .assertRenders(Box(.topLeft(), Size(width: 3, height: 3), border: border, background: "x"), "!_@\n>x<\n#-$")
+        expect("background only")
+            .assertRenders(Box(.topLeft(), Size(width: 3, height: 3), background: "x"), "xxx\nxxx\nxxx")
+
+        expect("outputs components, no border")
+            .assertRenders(Box(.topLeft(), Size(width: 5, height: 5), background: "x", components: [
+                LabelView(.middleCenter(), text: "hi!")
+                ]), "xxxxx\nxxxxx\nxhi!x\nxxxxx\nxxxxx")
+        expect("outputs components, with border")
+            .assertRenders(Box(.topLeft(), Size(width: 5, height: 5), border: .single, background: "_", components: [
+                LabelView(.middleCenter(), text: "hi!")
+                ]), "┌───┐\n│___│\n│hi!│\n│___│\n└───┘")
+        done()
+    }
+}
