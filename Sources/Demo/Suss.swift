64abac25cfed795e3c099065f388e7ff26b0023e
Colin T.A. Gray <colinta@gmail.com>
2017-08-10 12:26:44 -0600

    work on suss

--- Sources/Demo/Suss.swift
+++ Sources/Demo/Suss.swift
@@ -19,10 +19,13 @@ struct Suss: Program {
     struct Model {
         enum Input: Int {
             static let first: Input = .url
-            static let last: Input = .method
+            static let last: Input = .headers
 
             case url
-            case method
+            case httpMethod
+            case parameters
+            case body
+            case headers
 
             var next: Input { return Input(rawValue: rawValue + 1) ?? .first }
             var prev: Input { return Input(rawValue: rawValue - 1) ?? .last }
@@ -31,6 +34,9 @@ struct Suss: Program {
         var active: Input = .url
         var url: String = "https://"
         var httpMethod: String = "GET"
+        var parameters: [String] = []
+
+        var status: String { return "[Suss v1.0.0]"}
 
         var nextMethod: String {
             switch httpMethod {
@@ -81,7 +87,6 @@ struct Suss: Program {
         case .quit:
             return (model, [], .quit)
         case .submit:
-            // model.url = model.url + model.url
             break
         case .nextInput:
             model.active = model.active.next
@@ -114,48 +119,61 @@ struct Suss: Program {
                 return Message.submit
             })
 
-        let methods = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"]
+        let httpMethods = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"]
         let activeAttrs: [Attr]
-        if model.active == .method {
+        if model.active == .httpMethod {
             activeAttrs = [.reverse]
         }
         else {
             activeAttrs = [.underline]
         }
 
-        let methodText: [TextType] = methods.map { method -> Text in
-            return Text(method, attrs: method == model.httpMethod ? activeAttrs : [])
-        }.reduce([TextType]()) { (memo, methodText) -> [TextType] in
+        let httpMethodText: [TextType] = httpMethods.map { httpMethod -> Text in
+            return Text(httpMethod, attrs: httpMethod == model.httpMethod ? activeAttrs : [])
+        }.reduce([TextType]()) { (memo, httpMethodText) -> [TextType] in
             if memo.count > 0 {
-                return memo + [" ", methodText]
+                return memo + [" ", httpMethodText]
             }
             else {
-                return [methodText]
+                return [httpMethodText]
             }
         }
-        let methodInputs: [Component]
-        if model.active == .method {
-            methodInputs = [
-                LabelView(text: AttrText(methodText)),
+        let httpMethodInputs: [Component]
+        if model.active == .httpMethod {
+            httpMethodInputs = [
+                LabelView(text: AttrText(httpMethodText)),
                 OnKeyPress(.key_left, { return Message.prevMethod }),
                 OnKeyPress(.key_right, { return Message.nextMethod }),
             ]
         }
         else {
-            methodInputs = [LabelView(text: AttrText(methodText))]
+            httpMethodInputs = [LabelView(text: AttrText(httpMethodText))]
         }
 
+        let urlLabel = Text("URL", attrs: (model.active == .url ? [.reverse] : []))
+        let methodLabel = Text("Method", attrs: (model.active == .httpMethod ? [.reverse] : []))
+        let bodyLabel = Text("POST Body", attrs: (model.active == .body ? [.reverse] : []))
+        let headersLabel = Text("Headers", attrs: (model.active == .headers ? [.reverse] : []))
+
+        let parametersLabel = Text("GET Parameters", attrs: (model.active == .parameters ? [.reverse] : []))
+        let parameters: [Component] = model.parameters.enumerated().map { yOffset, param in
+            return LabelView(.topLeft(x: 0, y: yOffset), text: param)
+        }
+
+        let maxSideWidth = 30
         let remainingHeight = max(screenSize.height - 8, 0)
         return Window(components: [
             OnKeyPress(.key_esc, { return Message.quit }),
             OnKeyPress(.key_tab, { return Message.nextInput }),
             OnKeyPress(.key_backtab, { return Message.prevInput }),
-            Box(.topLeft(x: 0, y: 0), Size(width: screenSize.width, height: 2), border: fullBorder, components: [urlInput]),
-            LabelView(.topLeft(x: 2, y: 0), text: "URL"),
-            Box(.topLeft(x: 0, y: 3), Size(width: 38, height: 2), border: sideBorder, components: methodInputs),
-            LabelView(.topLeft(x: 2, y: 3), text: "Method"),
-            Box(.topLeft(x: 0, y: 6), Size(width: min(20, screenSize.width / 3), height: remainingHeight), border: sideBorder),
-            LabelView(.topLeft(x: 2, y: 6), text: "Parameters"),
+            Box(.topLeft(x: 0, y: 0), Size(width: screenSize.width, height: 2), border: fullBorder, label: urlLabel, components: [urlInput]),
+            Box(.topLeft(x: 0, y: 3), Size(width: 38, height: 2), border: sideBorder, label: methodLabel, components: httpMethodInputs),
+            GridLayout(.topLeft(x: 0, y: 6), Size(width: min(maxSideWidth, screenSize.width / 3), height: remainingHeight), rows: [
+                .row([Box(border: sideBorder, background: "-", label: parametersLabel, components: parameters, scrollOffset: Point.zero)]),
+                .row([Box(border: sideBorder, background: "=", label: bodyLabel)]),
+                .row([Box(border: sideBorder, background: "~", label: headersLabel)]),
+            ]),
+            Box(.bottomRight(x: 0, y: -1), Size(width: screenSize.width, height: 1), background: Text(" ", attrs: [.reverse]), components: [LabelView(text: Text(model.status, attrs: [.reverse]))]),
         ])
     }
 }
