c9615b7fe33f249fb0a354f8de47eb1425867f6e
Colin T.A. Gray <colinta@gmail.com>
2017-08-15 08:45:59 -0600

    fix clipping issue - don't allow drawing outside current offset

--- Sources/Core/Buffer.swift
+++ Sources/Core/Buffer.swift
@@ -6,6 +6,7 @@ class Buffer {
     typealias Chars = [Int: [Int: AttrCharType]]
     private(set) var chars: Chars = [:]
     private var offset: Point = .zero
+    private var zeroOffset: Point = .zero
     private var size: Size
 
     init(size: Size) {
@@ -13,34 +14,40 @@ class Buffer {
     }
 
     func push(offset nextOffset: Point, clip nextDesiredSize: Size, _ block: () -> Void) {
-        guard nextOffset.x < size.width && nextOffset.y < size.height else { return }
+        guard nextOffset.x < size.width, nextOffset.y < size.height else { return }
         let nextSize = Size(width: min(size.width - nextOffset.x, nextDesiredSize.width), height: min(size.height - nextOffset.y, nextDesiredSize.height))
-        guard nextOffset.x + nextSize.width >= 0 && nextOffset.y + nextSize.height >= 0 else { return }
-        guard nextSize.width > 0 && nextSize.height > 0 else { return }
+        guard nextOffset.x + nextSize.width >= 0, nextOffset.y + nextSize.height >= 0 else { return }
+        guard nextSize.width > 0, nextSize.height > 0 else { return }
 
         let prevOffset = offset
+        let prevZeroOffset = zeroOffset
         let prevSize = size
         offset = Point(
             x: offset.x + nextOffset.x,
             y: offset.y + nextOffset.y
             )
+        zeroOffset = Point(
+            x: max(zeroOffset.x, offset.x),
+            y: max(zeroOffset.y, offset.y)
+            )
         size = nextSize
         block()
         offset = prevOffset
+        zeroOffset = prevZeroOffset
         size = prevSize
     }
 
     func write(_ char: AttrCharType, x localX: Int, y localY: Int) {
         guard
-            localX >= 0 && localY >= 0 &&
-            localX < size.width && localY < size.height &&
-            char.string != ""
+            char.string != "",
+            localX >= 0, localY >= 0,
+            localX < size.width, localY < size.height
         else { return }
 
         let x = localX + offset.x
         let y = localY + offset.y
         guard
-            x >= 0 && y >= 0
+            x >= 0, y >= 0, x >= zeroOffset.x, y >= zeroOffset.y
         else { return }
 
         var row = chars[y] ?? [:]
