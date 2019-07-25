41471d8654d583db1c7b729d93defa40e94d78e0
Colin T.A. Gray <colinta@gmail.com>
2019-07-25 12:38:04 -0400

    migrate to Swift package manager, remove xcodeproj

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Ashen",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_14),
     ],
     products: [
         .library(name: "Ashen", targets: ["Ashen"]),
