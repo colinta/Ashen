1f99cf0beea21a55794b9710209c18a9f8341a06
Colin T.A. Gray <colinta@gmail.com>
2019-08-13 11:34:33 -0400

    update Box specs

--- Tests/AshenTests/BoxSpecs.swift
+++ Tests/AshenTests/BoxSpecs.swift
@@ -19,42 +19,68 @@ struct BoxSpecs: Spec {
             tbSide: "-", lrSide: "|"
             )
         expect("outputs nothing")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 0, height: 0), border: border, background: "x"), "", "when width,height == 0")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 1, height: 0), border: border, background: "x"), "", "when height == 0")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 0, height: 1), border: border, background: "x"), "", "when width == 0")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 0, height: 0), border: border, background: "x"), "", "when width,height == 0")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 1, height: 0), border: border, background: "x"), "", "when height == 0")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 0, height: 1), border: border, background: "x"), "", "when width == 0")
         expect("outputs dot")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 1, height: 1), border: border, background: "x"), ".")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 1, height: 1), border: borderDefaults, background: "x"), "!", "defaults")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 1, height: 1), border: border, background: "x"), ".")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 1, height: 1), border: borderDefaults, background: "x"), "!", "defaults")
         expect("outputs top/bottom caps")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 1, height: 3), border: border, background: "x"), "T\n|\nU")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 1, height: 3), border: borderDefaults, background: "x"), "!\n|\n#", "defaults")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 1, height: 3), border: border, background: "x"), "T\n|\nU")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 1, height: 3), border: borderDefaults, background: "x"), "!\n|\n#", "defaults")
         expect("outputs left/right caps")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 3, height: 1), border: border, background: "x"), "L=R")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 3, height: 1), border: borderDefaults, background: "x"), "!-@", "defaults")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 3, height: 1), border: border, background: "x"), "L=R")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 3, height: 1), border: borderDefaults, background: "x"), "!-@", "defaults")
         expect("outputs corners")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 2, height: 2), border: border, background: "x"), "!@\n#$")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 2, height: 2), border: border, background: "x"), "!@\n#$")
         expect("outputs box with background")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 3, height: 3), border: border, background: "x"), "!_@\n>x<\n#-$")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 3, height: 3), border: border, background: "x"), """
+            !_@
+            >x<
+            #-$
+            """)
         expect("background only")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 3, height: 3), background: "x"), "xxx\nxxx\nxxx")
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 3, height: 3), background: "x"), """
+            xxx
+            xxx
+            xxx
+            """)
 
         expect("outputs components, no border")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 5, height: 5), background: "x", components: [
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 5, height: 5), background: "x", components: [
                 LabelView(at: .middleCenter(), text: "hi!")
-                ]), "xxxxx\nxxxxx\nxhi!x\nxxxxx\nxxxxx")
+                ]), """
+                xxxxx
+                xxxxx
+                xhi!x
+                xxxxx
+                xxxxx
+                """)
         expect("outputs components, with border")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 5, height: 5), border: .single, background: "_", components: [
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 5, height: 5), border: .single, background: "_", components: [
                 LabelView(at: .middleCenter(), text: "hi!")
-                ]), "┌───┐\n│___│\n│hi!│\n│___│\n└───┘")
+                ]), """
+                ┌───┐
+                │___│
+                │hi!│
+                │___│
+                └───┘
+                """)
         expect("outputs scrolled components, with border")
-            .assertRenders(Box(at: .topLeft(), size: Size(width: 5, height: 5), border: .doubleSides, background: "_", components: [
+            .assertRenders(Box(at: .topLeft(), size: DesiredSize(width: 5, height: 5), border: .doubleSides, background: "_", components: [
                     LabelView(at: .topLeft(x: 0, y: 0), text: "abcdefghij"),
                     LabelView(at: .topLeft(x: 0, y: 1), text: "ABCDEFGHIJ"),
                     LabelView(at: .topLeft(x: 0, y: 2), text: "1234567890"),
                     LabelView(at: .topLeft(x: 0, y: 3), text: "klmnopqrst"),
                     LabelView(at: .topLeft(x: 0, y: 4), text: "KLMNOPQRST"),
                     LabelView(at: .topLeft(x: 0, y: 5), text: "!@#$%^&*()"),
-                ], scrollOffset: Point(x: 1, y: 1)), "╓───╖\n║BCD║\n║234║\n║lmn║\n╙───╜")
+                ], scrollOffset: Point(x: 1, y: 1)), """
+                ╓───╖
+                ║BCD║
+                ║234║
+                ║lmn║
+                ╙───╜
+                """)
         done()
     }
 }
