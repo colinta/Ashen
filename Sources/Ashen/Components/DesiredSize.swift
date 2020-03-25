c3636ac43595a863e9c0f2b5aa748e4e2f47506e
Colin T.A. Gray <colinta@gmail.com>
2020-03-25 12:48:17 -0400

    support 'calculatable' sizes

--- Sources/Ashen/Components/DesiredSize.swift
+++ Sources/Ashen/Components/DesiredSize.swift
@@ -9,6 +9,11 @@ public struct DesiredSize {
     static public let zero = DesiredSize(width: 0, height: 0)
     static public let max = DesiredSize(width: .max, height: .max)
 
+    public init(calculate: @escaping (Size) -> Size) {
+        self.width = .calculate({ size in calculate(size).width })
+        self.height = .calculate({ size in calculate(size).height })
+    }
+
     public init(width: Dimension? = nil, height: Dimension? = nil) {
         self.width = width
         self.height = height
@@ -35,29 +40,32 @@ public struct DesiredSize {
     }
 
     func constrain(in size: Size) -> Size {
-        let width = constrain(self.width, in: size.width)
-        let height = constrain(self.height, in: size.height)
+        let width = constrain(self.width, in: size, max: size.width)
+        let height = constrain(self.height, in: size, max: size.height)
         return Size(width: width, height: height)
     }
 
-    private func constrain(_ dimension: Dimension?, in size: Int) -> Int {
+    private func constrain(_ dimension: Dimension?, in size: Size, max: Int) -> Int {
         guard let dimension = dimension else { return 0 }
 
         switch dimension {
         case let .literal(literal):
             return literal
+        case let .calculate(calculate):
+            return calculate(size)
         case .max:
-            return size
+            return max
         case let .smallest(values):
-            return values.filter({ $0 < size }).min() ?? 0
+            return values.filter({ $0 <= max }).min() ?? 0
         case let .biggest(values):
-            return values.filter({ $0 < size }).max() ?? 0
+            return values.filter({ $0 <= max }).max() ?? 0
         }
     }
 }
 
 public enum Dimension {
     case literal(Int)
+    case calculate((Size) -> Int)
     case max
     case smallest([Int])
     case biggest([Int])
