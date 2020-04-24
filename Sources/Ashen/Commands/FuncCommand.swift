d3571aa5aa0e3f3917868ebb828b458a7f4b15e1
Colin T.A. Gray <colinta@gmail.com>
2020-04-24 12:44:24 -0400

    make msg return optional in FuncCommand

--- Sources/Ashen/Commands/FuncCommand.swift
+++ Sources/Ashen/Commands/FuncCommand.swift
@@ -4,23 +4,24 @@
 
 
 public struct FuncCommand {
-    let fn: () -> AnyMessage
+    let fn: () -> AnyMessage?
 }
 
 extension FuncCommand: Command {
-    public init(_ fn: @escaping () -> AnyMessage) {
+    public init(_ fn: @escaping () -> AnyMessage?) {
         self.fn = fn
     }
 
     public func start(_ send: @escaping (AnyMessage) -> Void) {
-        let msg = fn()
-        send(msg)
+        if let msg = fn() {
+            send(msg)
+        }
     }
 
     public func map<T, U>(_ mapper: @escaping (T) -> U) -> Self {
         let fn = self.fn
         return FuncCommand({
-            mapper(fn() as! T)
+            fn().map { mapper($0 as! T) }
         })
     }
 }
