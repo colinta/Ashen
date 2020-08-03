0f09aac8b17ee741edca31981f59228b54c41179
Colin T.A. Gray <colinta@gmail.com>
2020-08-03 14:07:22 -0400

    Adds ZStack view

---
+++ Sources/Ashen/Views/ZStack.swift
@@ -0,0 +1,38 @@
+////
+///  ZStack.swift
+//
+
+public func ZStack<Msg>(_ views: View<Msg>...) -> View<Msg> {
+    ZStack(views)
+}
+
+public func ZStack<Msg>(_ views: [View<Msg>]) -> View<Msg> {
+    View<Msg>(
+        preferredSize: { size in
+            views.reduce(Size.zero) { memo, view in
+                let viewSize = view.preferredSize(size)
+                return Size(
+                    width: max(memo.width, viewSize.width),
+                    height: max(memo.height, viewSize.height)
+                )
+            }
+        },
+        render: { rect, buffer in
+            for (index, view) in views.enumerated() {
+                buffer.render(
+                    key: index, view: view, at: .zero,
+                    clip: rect.size)
+            }
+        },
+        events: { event, buffer in
+            views.enumerated().reduce(([Msg](), [event])) { info, index_view in
+                let (msgs, events) = info
+                let (index, view) = index_view
+                let (newMsgs, newEvents) = View.scan(events: events) { event in
+                    return buffer.events(key: index, event: event, view: view)
+                }
+                return (msgs + newMsgs, newEvents)
+            }
+        }
+    )
+}
